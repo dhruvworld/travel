c3b636d12401e4ea49d65fed00e25c432bb8f094
dhruvworld <dhruv4701@gmail.com>
2025-03-28 15:32:15 -0400

    free-11

--- import NextAuth from "next-auth";.ts
+++ import NextAuth from "next-auth";.ts
@@ -1,6 +1,8 @@
 import NextAuth from "next-auth";
-import { authOptions } from "../../../config"; // ← FIXED: goes up 3 folders to reach root
+import { authOptions } from "config.ts"; // ← FIXED: goes up 3 folders to reach root
 
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
+
+
