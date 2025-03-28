17ff8996d83799d7cd4a2ce55a895ed33ad708e5
dhruvworld <dhruv4701@gmail.com>
2025-03-28 13:02:10 -0400

    test-11

--- next.config.js
+++ next.config.js
@@ -1,21 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Optimize build output
   output: 'standalone',
   
-  // Improve build reliability
-  onError: async (err) => {
-    console.error('Next.js build error:', err);
-  },
-  
-  // Enable webpack optimization
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+  webpack: (config) => {
     // Add build optimizations
     config.optimization = {
       ...config.optimization,
       minimize: true,
     }
-    
     return config
   },
 
