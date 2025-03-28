4a70ae2d531247ca36592bcb515b8d38701f80ba
dhruvworld <dhruv4701@gmail.com>
2025-03-28 14:53:44 -0400

    free-6

--- config/auth.ts
+++ config/auth.ts
@@ -1,35 +1,17 @@
-import { AuthOptions, Session, User } from "next-auth";
+import { AuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
-import { JWT } from "next-auth/jwt";
 
 export const authOptions: AuthOptions = {
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!,
+      clientId: process.env.GITHUB_ID || "",
+      clientSecret: process.env.GITHUB_SECRET || "",
     }),
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
     }),
   ],
-  callbacks: {
-    async jwt({ token, user }: { token: JWT, user: User }) {
-      if (user?.role) {
-        token.role = user.role;
-      }
-      return token;
-    },
-    async session({ session, token }: { session: Session, token: JWT }): Promise<Session> {
-      if (session?.user) {
-        session.user.role = token.role;
-      }
-      return session;
-    }
-  },
-  pages: {
-    signIn: '/auth/signin',
-  },
   secret: process.env.NEXTAUTH_SECRET,
 };
