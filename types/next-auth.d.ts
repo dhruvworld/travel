4950744152a0f37e9f7c9e23e39cadfc0270337b
dhruvworld <dhruv4701@gmail.com>
2025-03-28 14:16:27 -0400

    free-3

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,17 +1,19 @@
 import "next-auth";
+import { JWT } from "next-auth/jwt";
 
 declare module "next-auth" {
   interface User {
     role?: string;
-    // Add other custom properties here
   }
 
   interface Session {
-    user: User & {
+    user: {
       role?: string;
-    };
+    } & DefaultSession["user"]
   }
+}
 
+declare module "next-auth/jwt" {
   interface JWT {
     role?: string;
   }
