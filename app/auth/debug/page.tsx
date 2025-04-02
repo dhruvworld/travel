4bdcd8fb1d7d6401e2005d80f2ffe2464c856f45
dhruvworld <dhruv4701@gmail.com>
2025-04-01 23:29:39 -0400

    free-30

--- app/auth/debug/page.tsx
+++ app/auth/debug/page.tsx
@@ -42,14 +42,14 @@ export default function AuthDebugPage() {
           onClick={() => signIn()} 
           className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
         >
-          Sign In
+          Sign&apos; In
         </button>
         
         <button 
           onClick={() => signOut()} 
           className="px-4 py-2 bg-red-600 text-white rounded hover:bg-red-700"
         >
-          Sign Out
+          Sign&apos; Out
         </button>
         
         <button 
