8b87a00787cc6b41dddf8b73ef238ec980a20ac6
dhruvworld <dhruv4701@gmail.com>
2025-04-01 23:38:50 -0400

    free-32

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,6 +1,6 @@
 // lib/auth.ts
 import { getServerSession } from 'next-auth';
-import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { authOptions } from '@/app/lib/auth-options';
 import type { Session } from 'next-auth';
 import { redirect } from 'next/navigation';
 
